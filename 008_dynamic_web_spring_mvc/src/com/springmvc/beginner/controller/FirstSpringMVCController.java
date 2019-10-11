c4f94aa8e931539e257fc8e1dd3a15bb30382723
Prafful Daga <prraful@gmail.com>
2019-10-11 11:01:22 +0530

    spring core with build path and spring mvc

---
+++ 008_dynamic_web_spring_mvc/src/com/springmvc/beginner/controller/FirstSpringMVCController.java
@@ -0,0 +1,23 @@
+package com.springmvc.beginner.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/view")
+public class FirstSpringMVCController {
+	
+	@RequestMapping("/")
+	public String sayHello() {
+		/**
+		 * It will look for handler mapping and use prefix + belowstring + suffix
+		 * to create the file path.
+		 * It will use the file path to send back as response.
+		 */
+		 // /WEB-INF/myviews/hello.jsp  
+		return "hello";
+	}
+	
+	
+
+}
